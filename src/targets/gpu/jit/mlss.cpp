b84f3f7b5fa2af7f2037efccc54a6216e4fb87eb
ozhang_amdeng <owen.zhang@amd.com>
2026-01-27 21:37:13 -0500

    Update to use MHA kernel

--- src/targets/gpu/jit/mlss.cpp
+++ src/targets/gpu/jit/mlss.cpp
@@ -28,10 +28,11 @@
 #include <migraphx/gpu/compile_gen.hpp>
 // #include <amdmlss/amdmlss_api.h>
 #include <migraphx/gpu/code_object_op.hpp>
-#include <migraphx/gpu/mcd.hpp>
+// #include <migraphx/gpu/mcd.hpp>
+#include <migraphx/gpu/shaderBinRel.hpp>
 
 #include <cctype>
-
+#include <fstream>
 namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
 namespace gpu {
@@ -70,15 +71,33 @@ struct mlss_compiler : compiler<mlss_compiler>
 
         float scale = v.at("scale").to<float>();
 
-        std::string_view kernelName = group_query_attention_typed_unpacked_double_pointer_128_64x128x80_64x80x64_gfx1201.m_kernelName;
-        std::array<std::uint8_t, 61912> binaryData = group_query_attention_typed_unpacked_double_pointer_128_64x128x80_64x80x64_gfx1201.m_binary;
+        std::string_view kernelName = multi_head_attention_unpacked_128_64x192x48_64x48x64_forward_no_strides_fp16_gfx1201.m_kernelName;
+        std::array<std::uint8_t, 64880> binaryData = multi_head_attention_unpacked_128_64x192x48_64x48x64_forward_no_strides_fp16_gfx1201.m_binary;
 
         const auto& binary = binaryData[0];
 
         std::string kernel_name = std::string(kernelName);
-        size_t bin_size = 61912;
+        size_t bin_size = 64880;
 
-        value::binary value_binary(binaryData.data(), bin_size);
+        constexpr const char* module_file_name = "D:\\owen\\ModelInferencingScripts\\hip\\amdmlss_kernels\\mha\\multi_head_attention_unpacked_128_64x192x48_64x48x64_forward_no_strides_fp16-hip-amdgcn-amd-amdhsa-gfx1201.out";
+        std::ifstream file(module_file_name, std::ios::binary | std::ios::ate);
+        if(!file.is_open())
+        {
+            std::cout << "Failed to open file: " << module_file_name << "\n";
+        }
+
+        std::streamsize bin_size_file = file.tellg();
+        file.seekg(0, std::ios::beg);
+
+        std::vector<uint8_t> buffer(bin_size_file);
+
+        if(!file.read(reinterpret_cast<char*>(buffer.data()), bin_size_file))
+        {
+            throw std::runtime_error("Failed to read file");
+        }
+        bin_size = bin_size_file;
+        //value::binary value_binary(binaryData.data(), bin_size);
+        value::binary value_binary(buffer.data(), bin_size);
 
         auto nelements  = inputs.back().elements();
         auto block_size = compute_block_size(ctx, nelements, 256);
--- src/targets/gpu/kernel.cpp
+++ src/targets/gpu/kernel.cpp
@@ -22,6 +22,8 @@
  * THE SOFTWARE.
  */
 #include <migraphx/gpu/kernel.hpp>
+//#include <migraphx/gpu/half.hpp>
+
 #include <migraphx/manage_ptr.hpp>
 #include <migraphx/errors.hpp>
 #include <migraphx/gpu/pack_args.hpp>
@@ -139,16 +141,26 @@ void kernel::launch(hipStream_t stream,
     assert(impl != nullptr);
     void* kernargs   = reinterpret_cast<void*>(args.data());
     std::size_t size = args.bytes();
-
-    if (args.size() == 5)
+    if(args.size() == 5)
     {
-        using migraphx::half;
+         using migraphx::half;
 
-        size_t size_bytes = 1 * sizeof(half);
+         size_t size_bytes = 1 * sizeof(half);
 
-        std::vector<half> in_q{half{1.2}};
-        std::vector<half> in_k{half{1.2}};
-        std::vector<half> in_v{half{1.2}};
+         std::vector<half> in_q{half{2.0}};
+         std::vector<half> in_k{half{3.0}};
+         std::vector<half> in_v{half{4.0}};
+
+        //size_t size_bytes = 1 * sizeof(half_float::half);
+        //std::vector<half_float::half> in_q(size);
+        //std::vector<half_float::half> in_k(size);
+        //std::vector<half_float::half> in_v(size);
+
+        // std::vector<half_float::half> h_out(size);
+
+        in_q[0] = 2.0f;
+        in_k[0] = 3.0f;
+        in_v[0] = 4.0f;
 
         // in_q
         half* d_q_in{};
@@ -181,16 +193,13 @@ void kernel::launch(hipStream_t stream,
         hipMalloc(&dd_ptr, sizeof(void*));
         hipMemcpy(dd_ptr, &d_out, sizeof(void*), hipMemcpyHostToDevice);
 
-        // other inputs
-        constexpr int q_sequence_length  = 1;
-        constexpr int kv_sequence_lenght = 1;
-        constexpr int head_dim           = 2;
-        constexpr int batch_size         = 2;
-        constexpr int q_head_num         = 2;
-        constexpr int kv_head_num        = 2;
-        constexpr float scale            = 0.158114f;
+        constexpr int batch_size      = 1;
+        constexpr int sequence_length = 16384;
+        constexpr int head_num        = 8;
+        constexpr int head_dim        = 40;
+        constexpr float scale         = 0.353553f;
 
-        size_t offset  = 0;
+        size_t offset        = 0;
         char new_k_args[256] = {};
 
         *(reinterpret_cast<void***>(&new_k_args[offset])) = dd_q_ptr;
@@ -203,29 +212,45 @@ void kernel::launch(hipStream_t stream,
         *(reinterpret_cast<void***>(&new_k_args[offset])) = dd_ptr;
         offset += sizeof(dd_ptr);
 
-        *(reinterpret_cast<int*>(&new_k_args[offset])) = q_sequence_length;
-        offset += sizeof(q_sequence_length);
-        *(reinterpret_cast<int*>(&new_k_args[offset])) = kv_sequence_lenght;
-        offset += sizeof(kv_sequence_lenght);
-        *(reinterpret_cast<int*>(&new_k_args[offset])) = head_dim;
-        offset += sizeof(head_dim);
-        *(reinterpret_cast<int*>(&new_k_args[offset])) = head_dim;
-        offset += sizeof(head_dim);
+        //*(reinterpret_cast<void***>(&new_k_args[offset])) = args[4];
+        // offset += sizeof(args[4]);
+
         *(reinterpret_cast<int*>(&new_k_args[offset])) = batch_size;
         offset += sizeof(batch_size);
-        *(reinterpret_cast<int*>(&new_k_args[offset])) = q_head_num;
-        offset += sizeof(q_head_num);
-        *(reinterpret_cast<int*>(&new_k_args[offset])) = kv_head_num;
-        offset += sizeof(kv_head_num);
+        *(reinterpret_cast<int*>(&new_k_args[offset])) = sequence_length;
+        offset += sizeof(sequence_length);
+        *(reinterpret_cast<int*>(&new_k_args[offset])) = sequence_length;
+        offset += sizeof(sequence_length);
+        *(reinterpret_cast<int*>(&new_k_args[offset])) = head_num;
+        offset += sizeof(head_num);
+        *(reinterpret_cast<int*>(&new_k_args[offset])) = head_dim;
+        offset += sizeof(head_dim);
         *(reinterpret_cast<float*>(&new_k_args[offset])) = scale;
         offset += sizeof(scale);
 
-        launch_kernel(impl->fun, stream, global, local, new_k_args, offset, start, stop);
+        // constexpr const char* module_file_name =
+        // "D:\\owen\\ModelInferencingScripts\\hip\\amdmlss_kernels\\mha\\multi_head_attention_unpacked_128_64x192x48_64x48x64_forward_no_strides_fp16-hip-amdgcn-amd-amdhsa-gfx1201.out";
+
+        // hipModule_t module_new;
+        // auto status = hipModuleLoad(&module_new, module_file_name);
+
+        // if(status != hipSuccess)
+        //     MIGRAPHX_THROW("Failed to load module: " + hip_error(status));
+
+        // hipFunction_t kernel_new;
+        // auto status2 = hipModuleGetFunction(&kernel_new, module_new,
+        // "_ZN2ck16tensor_operation6device41kernel_multi_head_attention_wmma_unpackedINS1_49DeviceBatchedGemmSoftmaxGemmPermute_Wmma_CShuffleILi2ELi1ELi1ELi1ELi1EDF16_DF16_DF16_DF16_NS_5TupleIJEEEfS5_ffNS0_12element_wise11PassThroughES7_NS6_5ScaleES7_S7_LNS1_18GemmSpecializationE15ELNS1_20TensorSpecializationE0ELSA_0ELSA_0ELSA_0ELi1ELi128ELi64ELi192ELi48ELi8ELi8ELi48ELi64ELi8ELi16ELi16ELi16ELi1ELi12ELi3ENS_8SequenceIJLi2ELi64ELi1EEEENSB_IJLi1ELi0ELi2EEEESD_Li2ELi8ELi8ELb1ESC_SD_SD_Li2ELi8ELi8ELb1ENSB_IJLi2ELi8ELi8EEEENSB_IJLi0ELi2ELi1EEEESF_Li1ELi2ELi1ELb0ELi1ELi1ENSB_IJLi1ELi64ELi1ELi2EEEELi8ELNS1_21MaskingSpecializationE0ELNS_13LoopSchedulerE0ELNS_15PipelineVersionE0EEENS_35GridwiseBatchedGemmSoftmaxGemm_WmmaIDF16_DF16_fDF16_ffDF16_S7_S7_S8_S7_S7_LNS_25InMemoryDataOperationEnumE0ENS_16TensorDescriptorINS4_IJNS_5EmbedINS4_IJiiEEESP_Lb0EEENS_23Merge_v2_magic_divisionINS4_IJiEEEEEST_NS_8RightPadIiiLb0EEESV_NS_7UnMergeINS4_IJiNS_17integral_constantIiLi1EEENSX_IiLi2EEENSX_IiLi8EEEEEELb0EEENSW_INS4_IJiNSX_IiLi4EEENSX_IiLi16EEEEEELb0EEEEEENS4_IJNSB_IJLi0EEEENSB_IJLi1EEEENSB_IJLi2EEEENSB_IJLi3EEEENSB_IJLi4EEEENSB_IJLi6EEEENSB_IJLi5EEEEEEENS4_IJNSB_IJLi1ELi2EEEES1B_S1C_S1E_S1D_NSB_IJLi7ELi8ELi9ELi10EEEENSB_IJLi11ELi12ELi13EEEEEEENSB_IJLi7ELi11ELi12ELi8ELi9ELi13ELi10EEEExEENSN_INS4_IJSQ_ST_ST_SV_SV_NSW_INS4_IJiS10_EEELb0EEENS_11PassThroughIiEEEEES1F_NS4_IJS1G_S1B_S1C_S1E_S1D_NSB_IJLi7ELi8EEEENSB_IJLi9EEEEEEENSB_IJLi7ELi9ELi8EEEExEES1V_NSN_INS4_IJSQ_ST_ST_SV_SV_EEENS4_IJS18_S19_S1A_S1B_S1C_EEENS4_IJS1G_S1B_S1C_S1E_S1D_EEENSB_IJLi5ELi6EEEExEELi64ELi192ELi48ELi8ELi8ELi48ELi64ELi8ELi16ELi16ELi16ELi1ELi12ELi3ELi128ESC_SD_SD_Li2ELi8ELi8ELb1ELb0ELb1ESC_SD_SD_Li2ELi8ELi8ELb1ELb1ELb1ESE_SF_SF_Li1ELi2ELi1ELb0ELb1ELb0ELi1ELi1ESG_Li8ELb1ELb0ELi1ELSI_0ELSJ_0EEEDF16_DF16_DF16_DF16_S7_S7_S8_S7_S7_Lb0EEEvPPKT1_PPKT2_PPKT3_PPT4_iiiiif");
+
+        // if(status2 != hipSuccess)
+        //     MIGRAPHX_THROW("Failed to load module: " + hip_error(status));
+
+        launch_kernel(impl->fun, stream, 262144, local, new_k_args, offset, start, stop);
     }
     else
     {
         launch_kernel(impl->fun, stream, global, local, kernargs, size, start, stop);
     }
+    
 }
 
 void kernel::launch(hipStream_t stream,
@@ -240,6 +265,7 @@ void kernel::launch(hipStream_t stream,
     std::size_t size           = kernargs.size();
 
     launch_kernel(impl->fun, stream, global, local, kernargs.data(), size, start, stop);
+    
 }
 
 } // namespace gpu
